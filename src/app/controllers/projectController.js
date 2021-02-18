bad3ee905843f105915eb73a6df7677271525066
PauloMarquesrs <78260151+PauloMarquesrs@users.noreply.github.com>
2021-02-18 09:32:36 -0300

    'implementando-relacionamento-part_1'

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,11 +1,51 @@
 const express = require('express')
-const router = express.Router()
 const authMiddleware = require('../middlewares/auth')
+
+const Project = require('../models/Project')
+const Task = require('../models/Task')
+
+const router = express.Router()
 
 router.use(authMiddleware)
 
-router.get('/', (req, res) => {
-    res.send({ ok: true , user: req.userId })
+//list-----------------
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find().populate('user')
+ 
+        return res.send({ projects })
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading projects'})
+    
+    }
+})
+
+//show-----------------
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+//create---------------
+router.post('/', async (req, res) => {
+    try {
+        const project = await Project.create({ ...req.body, user: req.userId })
+
+        return res.send({ project })
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error creating new project'})
+    }
+})
+
+//update---------------
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+//delete---------------
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId})
 })
 
 module.exports = app => app.use('/projects', router)
